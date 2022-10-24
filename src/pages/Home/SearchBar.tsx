72ff3020f75858cdb999648bbf6d9fe3a18ac910
rowen <15138737638@163.com>
2022-10-24 15:29:18 +0800

    fix

--- src/pages/Home/SearchBar.tsx
+++ src/pages/Home/SearchBar.tsx
@@ -10,7 +10,8 @@ import { MenuFlyout } from 'components/Menu'
 import { AutoColumn } from 'components/Column'
 import { FixedSizeList } from 'react-window'
 import { LoadingRows } from 'components/Loader/styled'
-import { Line, TYPE } from 'theme'
+import { ExternalLink, Line, TYPE } from 'theme'
+import { Link } from 'react-router-dom'
 
 const SearchBarWraper = styled(Row)`
   width: 55%;
@@ -31,8 +32,29 @@ const SearchFlyout = styled(MenuFlyout)`
   background-color: ${({ theme }) => theme.bg0};
 `
 const SearchResWrapper = styled(AutoColumn)`
-  padding: 1rem;
+  padding: 0.6rem;
 `
+
+const ResultLink = styled(Link)`
+  text-decoration: none;
+  padding: 0.5rem 5px;
+  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.text5};
+  :hover {
+    background-color: ${({ theme }) => theme.bg1};
+  }
+`
+
+const ResultExternalLink = styled(ExternalLink)`
+  text-decoration: none;
+  padding: 0.5rem 5px;
+  display: flex;
+  :hover {
+    background-color: ${({ theme }) => theme.bg1};
+    text-decoration: none;
+  }
+`
+
 export default function SearchBar() {
   const node = useRef<HTMLDivElement>()
   const open = useModalIsOpen(ApplicationModal.HOME_SEARCH)
@@ -81,8 +103,8 @@ export default function SearchBar() {
             </LoadingRows>
           )}
           {debouncedQuery && (
-            <SearchResWrapper gap="16px">
-              <Row>
+            <SearchResWrapper>
+              <ResultLink to="/lido">
                 <svg viewBox="0 0 21 30" width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     fillRule="evenodd"
@@ -94,9 +116,8 @@ export default function SearchBar() {
                 <TYPE.body fontWeight={500} marginLeft={12}>
                   Lido
                 </TYPE.body>
-              </Row>
-              <Line />
-              <Row>
+              </ResultLink>
+              <ResultExternalLink href="">
                 <svg fill="#1d9bf0" viewBox="0 0 24 24" width={24} aria-hidden="true">
                   <g>
                     <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
@@ -105,8 +126,8 @@ export default function SearchBar() {
                 <TYPE.body fontWeight={500} marginLeft={12}>
                   Staked ETH issued by Lido is...
                 </TYPE.body>
-              </Row>
-              <Row>
+              </ResultExternalLink>
+              <ResultExternalLink href="">
                 <svg fill="#1d9bf0" viewBox="0 0 24 24" width={24} aria-hidden="true">
                   <g>
                     <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
@@ -115,8 +136,8 @@ export default function SearchBar() {
                 <TYPE.body fontWeight={500} marginLeft={12}>
                   Staked ETH issued by Lido is...
                 </TYPE.body>
-              </Row>
-              <Row>
+              </ResultExternalLink>
+              <ResultExternalLink href="">
                 <svg fill="#1d9bf0" viewBox="0 0 24 24" width={24} aria-hidden="true">
                   <g>
                     <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
@@ -125,7 +146,7 @@ export default function SearchBar() {
                 <TYPE.body fontWeight={500} marginLeft={12}>
                   Staked ETH issued by Lido is...
                 </TYPE.body>
-              </Row>
+              </ResultExternalLink>
             </SearchResWrapper>
           )}
         </SearchFlyout>
