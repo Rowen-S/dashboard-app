c219bb1fe760edfd6707c30eaab61018afa673b6
rowen <15138737638@163.com>
2022-12-29 11:46:26 +0800

    fix social icons

--- src/pages/BlockChain/Polkadot.tsx
+++ src/pages/BlockChain/Polkadot.tsx
@@ -66,7 +66,7 @@ const SocialLogo = styled.img`
 
 const ProjectSocial = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 15px;
 `
 
